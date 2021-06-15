5952d3571a1928016d7ec5250eef72f0aa77ee92
msarosh1 <54763184+msarosh1@users.noreply.github.com>
2021-06-15 17:53:00 +0500

    Adding a user interface for the todoList; functionality added as well.

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import 'primevue/resources/themes/saga-blue/theme.css'      //theme
-import 'primevue/resources/primevue.min.css'                 //core css
-import 'primeicons/primeicons.css'                           //icons
-import PrimeVue from 'primevue/config'
-import Dropdown from 'primevue/dropdown';
-import InputText from 'primevue/inputtext';
+import "primevue/resources/themes/saga-blue/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import PrimeVue from "primevue/config";
+import Dropdown from "primevue/dropdown";
+import InputText from "primevue/inputtext";
+import Editor from "primevue/editor";
 
-import GoogleAuth from './config/google_oAuth'
+import GoogleAuth from "./config/google_oAuth";
 const gauthOption = {
-  clientId: '980962916670-fu32uvl82sj0sel3n4j5ffoa1jn9nqdk.apps.googleusercontent.com',
-  scope: 'profile email',
-  prompt: 'select_account'
-}
-
+  clientId:
+    "980962916670-fu32uvl82sj0sel3n4j5ffoa1jn9nqdk.apps.googleusercontent.com",
+  scope: "profile email",
+  prompt: "select_account",
+};
 
 const app = createApp(App);
 
-app.use(GoogleAuth, gauthOption)
-app.config.productionTip = false
+app.use(GoogleAuth, gauthOption);
+app.config.productionTip = false;
 
-app.use(PrimeVue)
-app.component('Dropdown',Dropdown)
-app.component('InputText',InputText)
-app.mount('#app');
+app.use(PrimeVue);
+app.component("Editor", Editor);
+app.component("Dropdown", Dropdown);
+app.component("InputText", InputText);
+app.mount("#app");
 //createApp(App).mount('#app')
